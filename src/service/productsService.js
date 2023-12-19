ed873937221df2fea696d20241e92cfc8ecf3aa0
Alexis-Villalba <alevillalba3493@gmail.com>
2023-12-19 18:09:20 -0300

    identación y tabulación

--- src/service/productsService.js
+++ src/service/productsService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export async function getProducts(limit = 15) {
     return await axios.get(`https://fakestoreapi.com/products?limit=${limit}`)
-    
 }
+
 export async function getProductById(id) {
     return await axios.get(`https://fakestoreapi.com/products/${id}`)
 }
@@ -12,7 +12,7 @@ export async function getCategories() {
     return await axios.get(`https://fakestoreapi.com/products/categories`)
 }
 
-export async function getProductsByCategory(id ) {
+export async function getProductsByCategory(id) {
     return await axios.get(`https://fakestoreapi.com/products/category/${id}`)
-    
+
 }
